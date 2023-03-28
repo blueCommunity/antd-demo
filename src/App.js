1625b6a5cfcd3d4721f27936c326ccb44bf3c681
lijian08 <lijian08@qianxin.com>
2023-03-28 22:43:44 +1100

    0328

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card } from 'antd';
+import 'antd/dist/reset.css';
+import { Layout } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { Button, Input, Space } from 'antd';
 
+
+const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: 'rgb(176, 196, 222)'
+    }}>
+      <Card
+        hoverable
+        bordered={true}
+        style={{
+          width: '50%',
+        }}
+      >
+        <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+          <Input placeholder="input account number" />
+          <Input.Password
+            placeholder="input password"
+            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+          />
+          <Space>
+          <Button  >Login</Button>
+          <Button  type="link">register</Button>
+          </Space>
+        </Space>
+      </Card>
+
     </div>
   );
 }
